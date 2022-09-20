175e9aef5137df3d14adc1f113f392a9a1e03c16
Anne Dwojeski-Santos <dwojeskisantos.ims@gmail.com>
2022-09-20 01:42:20 +0000

    Week 3: Sketched out P1 mechanics; sep-of-concerns

---
+++ p1/index-view.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <title>Project 1</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+</head>
+  <body>
+    <h1>Project 1</h1>
+
+    <h2>Mechanics</h2>
+    <ul>
+      <li>I'd like to make a memory-style game.</li>
+      <li>(The memory feature will come in at a later stange when people are actually interacting with the app.)</li>
+      <li>There will be a 4x4 grid of cards, with eight word pairs.</li>
+      <li>Starting with Player A, the app will randomly select two cards. If they match, Player A gets to go again. This continues until they do not match.</li>
+      <li>When the cards do not match, Player A's turn ends and it's Player B's turn.</li>
+      <li>This continues until there are no pairs left. Whoever has the most pairs, wins. Ties are possible.</li>
+    </ul>
+
+    <h4>Note:</h4>
+    <ul>
+      <li>I'd like to make this a language-learning app, but I'm not sure how I'd check if the words matched.</li>
+      <li>That feature might be beyond my abilities for the first project.</li>
+    </ul>
+
+    <h2>Results</h2>
+    <ul>
+      <li>...</li>
+    </ul>
+</body>
+</html>
--- p1/index.php
+++ p1/index.php
@@ -1,22 +1,3 @@
-<!doctype html>
-<html lang='en'>
-<head>
-    <title>Project 1</title>
-    <meta charset='utf-8'>
-    <link href=data:, rel=icon>
-</head>
-  <body>
-    <h1>Project 1</h1>
+<?php
 
-    <h2>Mechanics</h2>
-    <ul>
-      <li>...</li>
-    </ul>
-
-    <h2>Results</h2>
-    <ul>
-      <li>...</li>
-    </ul>
-</body>
-</html>
-
+require "index-view.php";
