97b16eed5529c62e1adee7ddb71f39b0619e9a8d
Anne Dwojeski-Santos <dwojeskisantos.ims@gmail.com>
2022-09-25 12:44:33 +0000

    Initial set-up of Rainbow Deck project 1

--- p1/index-view.php
+++ p1/index-view.php
@@ -9,20 +9,28 @@
     <h1>Project 1</h1>
 
     <h2>Mechanics</h2>
-    <ul>
-      <li>I'd like to make a memory-style game.</li>
-      <li>(The memory feature will come in at a later stange when people are actually interacting with the app.)</li>
-      <li>There will be a 4x4 grid of cards, with eight word pairs.</li>
-      <li>Starting with Player A, the app will randomly select two cards. If they match, Player A gets to go again. This continues until they do not match.</li>
-      <li>When the cards do not match, Player A's turn ends and it's Player B's turn.</li>
-      <li>This continues until there are no pairs left. Whoever has the most pairs, wins. Ties are possible.</li>
-    </ul>
+      <ul>
+        <li>In this game, players try to build a rainbow out of a deck of rainbow cards.</li>
+      </ul>
+    <h3>Solo game</h3>
+      <ul>
+        <li>For my first pass, I'm going to do a solo game.</li>
+        <li>The cards are shuffled and the player is dealt cards one by one.</li>
+        <li>If a card is red, the player keeps it and starts to look for an orange card (and on through the rainbow).</li>
+        <li>If the card is not red, the player discards it and draws the next card.</li>
+        <li>In the solo game, I'm testing if the player can lose by not building a rainbow before getting through the deck.</li>
+        <li>I'm going to have a counter for how many draws it took to win.</li>
+        <li>I'd also like to do a test with 100 plays and see the average number of draws it takes.</li>
+      </ul>
 
-    <h4>Note:</h4>
-    <ul>
-      <li>I'd like to make this a language-learning app, but I'm not sure how I'd check if the words matched.</li>
-      <li>That feature might be beyond my abilities for the first project.</li>
-    </ul>
+    <h3>Two-player game</h3>
+      <ul>
+        <li>For the two-player game, the cards are shuffled and each person gets half of the deck.</li>
+        <li>Players take turns drawing a card and seeing if it's the next rainbow color they need.</li>
+        <li>PHASE 2: If yes, they get a second draw.</li>
+        <li>If it's not the next card they need, they put the card on the bottom of the deck.</li>
+        <li>The first player to build a rainbow wins.</li>
+      </ul>
 
     <h2>Results</h2>
     <ul>
--- p1/index.php
+++ p1/index.php
@@ -1,3 +1,37 @@
 <?php
 
 require "index-view.php";
+
+# Create array for the deck of cards.
+$deck = [
+  'red', 'red', 'red', 'red', 'red',
+  'orange', 'orange', 'orange', 'orange', 'orange',
+  'yellow', 'yellow', 'yellow', 'yellow', 'yellow',
+  'green', 'green', 'green', 'green', 'green',
+  'blue', 'blue', 'blue', 'blue', 'blue',
+  'indigo', 'indigo', 'indigo', 'indigo', 'indigo',
+  'violet', 'violet', 'violet', 'violet', 'violet'
+];
+// var_dump($deck[7]);
+
+# Create a win-condition array of ROYGBIV
+$rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+// var_dump($rainbow);
+
+# Create a $progress variable to keep track of where in this array we're matching to; start at 0.
+$progress = 0;
+
+# Shuffle deck
+shuffle($deck);
+// var_dump($deck);
+
+# Draw top card and look for match to 0, when that is met, +1 to $progress
+
+
+
+var_dump($progress);
+
+# Create a counter for the number of draws it takes.
+
+# Create a 100-game sequence and keep track of the number of draws from the counter for each game.
+# (Use an array and after each game, add the counter value to the array before starting the next game.)
