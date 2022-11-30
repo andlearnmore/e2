3e51c3defdd8906987855ac537ce0eca21b7e3b8
Anne Dwojeski-Santos <dwojeskisantos.ims@gmail.com>
2022-11-30 21:51:59 +0000

    Upated to pull data from database

--- zipfoods/app/Commands/AppCommand.php
+++ zipfoods/app/Commands/AppCommand.php
@@ -6,6 +6,11 @@ class AppCommand extends Command
 {
     public function test()
     {
-        dump('It works! You invoked your first command.');
+        dump('It works! You invoked your first command :) ');
+    }
+
+    public function migrate()
+    {
+        dump('You ran the migrate command');
     }
 }
--- zipfoods/app/Controllers/ProductsController.php
+++ zipfoods/app/Controllers/ProductsController.php
@@ -6,18 +6,9 @@ use App\Products;
 
 class ProductsController extends Controller 
 {
-    private $productsObj;
-
-    public function __construct($app)
-    {
-        parent::__construct($app);
-        $this->productsObj = new Products($this->app->path('/database/products.json'));
-    }
-
     public function index()
     {
-
-        $products = $this->productsObj->getAll();
+        $products = $this->app->db()->all('products'); 
 
         return $this->app->view('products/index', ['products' => $products]);
     }
@@ -26,10 +17,12 @@ class ProductsController extends Controller
     {
         $sku = $this->app->param('sku');
 
-        $product = $this->productsObj->getBySku($sku);
+        $productQuery = $this->app->db()->findByColumn('products', 'sku', '=', $sku);
  
-        if (is_null($product)){
+        if (empty($productQuery)){
             return $this->app->view('products/missing');
+        } else {
+            $product = $productQuery[0];
         }
 
         $reviewSaved = $this->app->old('reviewSaved');
@@ -37,7 +30,7 @@ class ProductsController extends Controller
         return $this->app->view('products/show', [
             'product' => $product, 
             'reviewSaved' => $reviewSaved 
-        ]);
+        ]); 
     }
 
     public function saveReview()
@@ -54,42 +47,12 @@ class ProductsController extends Controller
         $review = $this->app->input('review');
 
         # To do: persist to the database.
-        # Set up all the variables we need to make a connection
-        $host = $this->app->env('DB_HOST');
-        $database = $this->app->env('DB_NAME');
-        $username = $this->app->env('DB_USERNAME');
-        $password = $this->app->env('DB_PASSWORD');
-        
-        # DSN (Data Source Name) string
-        # contains the information required to connect to the database
-        $dsn = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-
-        # Driver-specific connection options
-        $options = [
-            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-            \PDO::ATTR_EMULATE_PREPARES => false,
-        ];
-
-        try {
-            # Create a PDO instance representing a connection to a database
-            $pdo = new \PDO($dsn, $username, $password, $options);
-        } catch (\PDOException $e) {
-            throw new \PDOException($e->getMessage(), (int)$e->getCode());
-        }
-
-        $sqlTemplate = "INSERT INTO reviews (name, sku, review) 
-            VALUES (:name, :sku, :review)";
-
-        $values = [
-            'name' => $name,
+        $this->app->db()->insert('reviews', [
             'sku' => $sku,
+            'name' => $name,
             'review' => $review
-        ];
-
-        $statement = $pdo->prepare($sqlTemplate);
-        $statement->execute($values);
-
+        ]);
+        
         return $this->app->redirect('/product?sku=' . $sku, ['reviewSaved'
          => true]); 
 
