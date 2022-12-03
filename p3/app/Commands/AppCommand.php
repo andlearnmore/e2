d1635f8c01c9c32006465f6116cdbe0955daed9f
Anne Dwojeski-Santos <dwojeskisantos.ims@gmail.com>
2022-12-03 18:52:31 +0000

    created migration

--- p3/app/Commands/AppCommand.php
+++ p3/app/Commands/AppCommand.php
@@ -8,4 +8,16 @@ class AppCommand extends Command
     {
         dump('It works! You invoked your first command.');
     }
+
+    public function migrate()
+    {
+        $this->app->db()->createTable('nouns', [
+            'noun' => 'varchar(255)',
+            'article' => 'varchar(3)',
+            'plural' => 'varchar(255)',
+            'english' => 'varchar(255)'
+        ]);
+
+        dump('Migration complete. Check the database for your new table(s).');
+    }
 }
